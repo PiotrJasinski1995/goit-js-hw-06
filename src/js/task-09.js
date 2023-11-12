aae5bdaf6a32844bf1f28057adbf07ae26de8e65
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2023-11-12 22:45:47 +0100

    feat: implement task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector("button.change-color");
+const colorText = document.querySelector("span.color");
+
+changeColorButton.addEventListener("click", (_event) => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+});
