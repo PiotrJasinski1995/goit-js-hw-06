f791bf81aed3c4fc6d5c641b1fab881f45d84b71
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2023-11-12 22:45:38 +0100

    feat: implement task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector("form.login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const formObject = {};
+
+  const { email, password } = form;
+
+  // formObject.email = email.value;
+  // formObject.password = password.value;
+
+  formObject.email = form.elements.email.value;
+  formObject.password = form.elements.password.value;
+
+  console.log(formObject);
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+    return;
+  }
+
+  form.reset();
+});
