a2c0610a2da0d2b2bcba73f9cb545bb3eb5c7037
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2023-11-12 22:44:43 +0100

    feat: implement task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const elementList = [];
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient, index) => {
+  const element = document.createElement("li");
+  element.className = "item";
+  element.textContent = ingredient;
+  elementList[index] = element;
+});
+
+ingredientsList.append(...elementList);
